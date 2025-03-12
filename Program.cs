4282ce15f89bcacc4c1879125986e1f651537c6d
Shahab <shahababdian301@example.com>
2025-03-12 23:57:54 +0330

    install larawel

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Getting temperature input from the user
+        Console.Write("Enter temperature in Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        // Calculating Fahrenheit
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        // Displaying the result
+        Console.WriteLine("Equivalent Fahrenheit temperature: " + fahrenheit);
+
+        // Displaying the equation
+        Console.WriteLine("Conversion equation: F = (9/5) * C + 32");
+    }
+}
